e2c35db98d6901e931339c657a3c66213ae892e3
Dor <dorzairi@ymail.com>
2023-11-22 20:34:02 -0500

    prediction now working and fixed collision issue

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,51 +1,50 @@
 extends Node2D
 
-var speed: float = 20.0
+var speed: float = 220.0
+var prediction_time: float = 0.5
 var player: Node2D
-var player_velocity: Vector2  # Assuming this is updated in your player script
+var player_velocity: Vector2
 var end_game_triggered: bool = false
 var current_velocity: Vector2 = Vector2()
 var is_seeking: bool = true
 
 func _ready():
 	print_debug("Hello From Enemy")
-	player = get_node("../Player")
-	$Area2D.connect("body_entered", Callable(self, "_on_body_entered"))
+	player = get_node("../Player")  # Ensure this correctly references the player node
+	self.area_entered.connect(_on_body_entered)  # Connect collision signal
+	set_process(true)
 
 func _physics_process(delta):
 	if player == null or end_game_triggered or not is_seeking:
 		return
-	player_velocity = player.get("current_velocity")  # Replace 'current_velocity' with the actual variable name in your player script
-	seek(player.global_position + player_velocity * delta, delta)
+
+	player_velocity = player.get("current_velocity")
+	var predicted_position = player.global_position + player_velocity * prediction_time
+	seek(predicted_position, delta)
 
 func seek(target_position: Vector2, delta: float) -> void:
 	var distance_to_player = global_position.distance_to(target_position)
+	var desired_velocity: Vector2 = (target_position - global_position).normalized() * speed
 
-	if distance_to_player <= 20:
-		current_velocity = Vector2.ZERO
-		global_position = target_position  # Snap to the exact position
-		_on_body_entered(player)
-		is_seeking = false  # Stop seeking
-		return
+	# Adjust the speed based on distance to the player
+	if distance_to_player < 50:  # This is the slowing radius, adjust as needed
+		var slowing_factor = distance_to_player / 50  # Slow down as it gets closer
+		desired_velocity *= slowing_factor
 
-	# Dynamically adjust speed based on distance to player
-	var slowing_radius: float = 20.0  # Radius within which to start slowing down
-	var adjusted_speed: float = speed * (distance_to_player / slowing_radius)
-	if adjusted_speed < 10.0:  # Minimum speed threshold
-		adjusted_speed = 10.0
-
-	var desired_velocity: Vector2 = (target_position - global_position).normalized() * adjusted_speed
-	var steering: Vector2 = desired_velocity - current_velocity
+	# Apply a steering force towards the desired velocity
+	var steering: Vector2 = (desired_velocity - current_velocity).normalized() * speed
 	current_velocity += steering * delta
+
+	# Update the position
 	global_position += current_velocity * delta
 
+
 func _on_body_entered(body: Node) -> void:
-	print("Body entered: ", body.name)
 	if body == player:
 		print("collided with player")
-		player.queue_free()
+		player.queue_free()  # Delete the player on collision
 		trigger_end_game()
 
 func trigger_end_game() -> void:
 	end_game_triggered = true
-	# Implement your end game sequence here
+	# Add any end game logic here
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,27 @@
 extends Node2D
 
 var speed = 400  # Speed in pixels per second
-var current_velocity = Vector2.ZERO  # Added this line to store the player's current velocity
+var current_velocity = Vector2.ZERO  # Store the player's current velocity
+var movement_smoothness = 0.1  # Adjust for smoother movement transitions
 
 func _ready():
-	call_deferred("set_name", "Player")  # Defer renaming the node to the next frame
+	call_deferred("set_name", "Player")
 
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	var to_mouse_vector = mouse_pos - global_position
 
 	# Make the player face the mouse
-	look_at(mouse_pos * -1)
+	look_at(mouse_pos)
 
 	# Move the player towards the mouse when the left mouse button is pressed
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if to_mouse_vector.length() > 10:
-			to_mouse_vector = to_mouse_vector.normalized() * speed
-			global_position += to_mouse_vector * delta
-			current_velocity = to_mouse_vector  # Update the current_velocity variable
+			var target_velocity = to_mouse_vector.normalized() * speed
+			current_velocity = current_velocity.lerp(target_velocity, movement_smoothness)
 		else:
-			current_velocity = Vector2.ZERO  # Reset the velocity if the player is not moving
+			current_velocity = Vector2.ZERO
+	else:
+		current_velocity = current_velocity.lerp(Vector2.ZERO, movement_smoothness)
+
+	global_position += current_velocity * delta
